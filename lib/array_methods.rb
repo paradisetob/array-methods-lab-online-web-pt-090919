f1857870cb23d620ebe3e5ec1612706d679f1211
tracey brimage <onelovenilynn@gmail.com>
2019-09-20 04:33:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def using_include(array, element)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
     array.include?("wow")
-  true
+  =>true
     array.include?("Tardis")
-    
+
     end
 
 def using_sort(array)
